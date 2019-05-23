33b6431513be6506c013d9f9e7b0d51a7d4eef15
Sara McCombs <theSaraMcCombs@gmail.com>
2019-05-22 22:57:12 -0400

    completed ruby on rails I exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/' => "home#top"
+  get 'top' => "home#top"
+  get 'about' => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
